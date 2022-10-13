a92865ff96ce55417490022f042961c7562dd4c3
Dennis Pham <dennis@dennispham.me>
2022-10-13 13:02:25 -0400

    Set up `minio` client for API.

--- silicon/__init__.py
+++ silicon/__init__.py
@@ -3,8 +3,10 @@ from typing import Callable
 
 import asyncpg
 from fastapi import APIRouter, FastAPI, Request, Response
+from minio import Minio
 
-from silicon.constants import DATABASE_URL, LogConfig
+from silicon.constants import (DATABASE_URL, DEBUG, S3_ACCESS_KEY,
+                               S3_SECRET_KEY, S3_URL, LogConfig)
 from silicon.routes import routers
 
 logging.config.dictConfig(LogConfig().dict())
@@ -24,8 +26,14 @@ app.include_router(app_router)
 
 @app.on_event("startup")
 async def start() -> None:
-    """Sets up the database connection."""
+    """Sets up the database connection and minio client."""
     app.state.pool = await asyncpg.create_pool(DATABASE_URL)
+    app.state.minio = Minio(
+        S3_URL,
+        access_key=S3_ACCESS_KEY,
+        secret_key=S3_SECRET_KEY,
+        secure=not DEBUG,
+    )
 
 
 @app.on_event("shutdown")
@@ -37,6 +45,8 @@ async def shutdown() -> None:
 @app.middleware("http")
 async def setup_request(request: Request, callnext: Callable) -> Response:
     """Gets the database connection for each request."""
+    request.state.minio = app.state.minio
+
     async with app.state.pool.acquire() as conn:
         request.state.conn = conn
         response = await callnext(request)
--- silicon/constants.py
+++ silicon/constants.py
@@ -4,6 +4,23 @@ from pydantic import BaseModel
 DEBUG = config("DEBUG", cast=bool, default=False)
 DATABASE_URL = config("DATABASE_URL")
 
+S3_URL = config("S3_URL")
+S3_ACCESS_KEY = config("S3_ACCESS_KEY")
+S3_SECRET_KEY = config("S3_SECRET_KEY")
+
+S3_BUCKET_NAME = "msds"
+S3_BUCKET_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {"AWS": "*"},
+            "Action": "s3:GetObject",
+            "Resource": f"arn:aws:s3:::{S3_BUCKET_NAME}/*",
+        },
+    ],
+}
+
 
 class LogConfig(BaseModel):
     """Logging configuration for the application."""
