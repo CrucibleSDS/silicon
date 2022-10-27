c0c6eba293613988f5bcb1dc09833460bd855549
Dennis Pham <dennis@dennispham.me>
2022-10-27 12:45:51 -0400

    Create production docker image for silicon.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM python:3.10.6-slim
+
+# Set pip to have no saved cache
+ENV PIP_NO_CACHE_DIR=false \
+    POETRY_VIRTUALENVS_CREATE=false \
+    MAX_WORKERS=10
+
+# Install poetry
+RUN pip install -U poetry
+
+# Create the working directory
+WORKDIR /silicon
+
+# Install project dependencies
+COPY pyproject.toml poetry.lock ./
+RUN poetry install --no-dev
+
+# Copy the Gunicorn config
+COPY ./gunicorn_conf.py /gunicorn_conf.py
+
+# Copy the source code in last to optimize rebuilding the image
+COPY . .
+
+EXPOSE 80
+
+# Start Gunicorn with Uvicorn workers.
+CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "-c", "/gunicorn_conf.py", "backend:app"]
---
+++ gunicorn_conf.py
@@ -0,0 +1,48 @@
+# Based off of https://github.com/tiangolo/uvicorn-gunicorn-docker
+# There is no 3.10 image yet
+import json
+import multiprocessing
+import os
+
+use_max_workers = int(os.getenv("MAX_WORKERS", "0"))
+web_concurrency = int(os.getenv("WEB_CONCURRENCY", "0"))
+workers_per_core = float(os.getenv("WORKERS_PER_CORE", "1"))
+if not web_concurrency:
+    web_concurrency = max(int(workers_per_core * multiprocessing.cpu_count()), 2)
+    if use_max_workers:
+        web_concurrency = min(web_concurrency, use_max_workers)
+
+host = os.getenv("HOST", "0.0.0.0")
+port = os.getenv("PORT", "80")
+bind = os.getenv("BIND") or f"{host}:{port}"
+
+# Gunicorn config variables
+workers = web_concurrency
+
+loglevel = os.getenv("LOG_LEVEL", "info")
+errorlog = os.getenv("ERROR_LOG")
+accesslog = os.getenv("ACCESS_LOG")
+
+graceful_timeout = int(os.getenv("GRACEFUL_TIMEOUT", "120"))
+timeout = int(os.getenv("TIMEOUT", "120"))
+keepalive = int(os.getenv("KEEP_ALIVE", "5"))
+
+forwarded_allow_ips = os.getenv("FORWARDED_ALLOW_IPS", "*")
+
+# For debugging and testing
+log_data = {
+    "loglevel": loglevel,
+    "workers": workers,
+    "bind": bind,
+    "graceful_timeout": graceful_timeout,
+    "timeout": timeout,
+    "keepalive": keepalive,
+    "errorlog": errorlog,
+    "accesslog": accesslog,
+    # Additional, non-gunicorn variables
+    "workers_per_core": workers_per_core,
+    "use_max_workers": use_max_workers,
+    "host": host,
+    "port": port,
+}
+print(json.dumps(log_data))
