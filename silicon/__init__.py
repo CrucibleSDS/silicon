fd91d3b95e54d9eb74ae6d24acf93470b16cb93a
Den4200 <dennis@dennispham.me>
2022-11-07 10:53:34 -0500

    Install and use SQLAlchemy instead of directly using asyncpg.

--- silicon/__init__.py
+++ silicon/__init__.py
@@ -4,9 +4,17 @@ from typing import Callable
 import asyncpg
 from fastapi import APIRouter, FastAPI, Request, Response
 from minio import Minio
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-from silicon.constants import (DATABASE_URL, DEBUG, S3_ACCESS_KEY,
-                               S3_SECRET_KEY, S3_URL, LogConfig)
+from silicon.constants import (
+    DATABASE_URL,
+    DEBUG,
+    S3_ACCESS_KEY,
+    S3_SECRET_KEY,
+    S3_URL,
+    LogConfig,
+)
 from silicon.routes import routers
 
 logging.config.dictConfig(LogConfig().dict())
@@ -27,7 +35,9 @@ app.include_router(app_router)
 @app.on_event("startup")
 async def start() -> None:
     """Sets up the database connection and minio client."""
-    app.state.pool = await asyncpg.create_pool(DATABASE_URL)
+    app.state.engine = create_async_engine(DATABASE_URL, echo=True)
+    app.state.async_session = sessionmaker(app.state.engine, expire_on_commit=False, class_=AsyncSession)
+
     app.state.minio = Minio(
         S3_URL,
         access_key=S3_ACCESS_KEY,
@@ -38,8 +48,8 @@ async def start() -> None:
 
 @app.on_event("shutdown")
 async def shutdown() -> None:
-    """Closes the database connection."""
-    await app.state.pool.close()
+    """Closes the database connections."""
+    await app.state.engine.dispose()
 
 
 @app.middleware("http")
@@ -47,9 +57,9 @@ async def setup_request(request: Request, callnext: Callable) -> Response:
     """Gets the database connection for each request."""
     request.state.minio = app.state.minio
 
-    async with app.state.pool.acquire() as conn:
-        request.state.conn = conn
+    async with app.state.async_session() as session:
+        request.state.db = session
         response = await callnext(request)
 
-    request.state.conn = None
+    request.state.db = None
     return response
