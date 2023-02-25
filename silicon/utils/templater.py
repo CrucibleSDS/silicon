926fec0b7b902875e592a5622992639c8378cf26
Brian Lu <me@greencappuccino.net>
2023-02-24 22:11:18 -0500

    pdf generation bytesio

--- silicon/utils/templater.py
+++ silicon/utils/templater.py
@@ -1,6 +1,8 @@
+from io import BytesIO
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader
+from pdflatex import pdflatex
 
 
 class Templater:
@@ -9,5 +11,10 @@ class Templater:
         self.env = Environment(loader=self.file_loader)
         self.template = self.env.get_template('template.tex')
 
-    def template_front_page(self, data: dict[str, any]) -> str:
+    def template_tex(self, data: dict[str, any]) -> str:
         return self.template.render(data=data)
+
+    def generate_pdf(self, data: dict[str, any]) -> BytesIO:
+        pdfl = pdflatex.PDFLaTeX.from_jinja_template(self.template, data=data)
+        pdf, log, cp = pdfl.create_pdf()
+        return BytesIO(pdf)
