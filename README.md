8ac090abce86a2038a2e2cb8914bfe00c5310561
Brian Lu <me@greencappuccino.net>
2023-04-04 08:07:04 -0400

    Support generating more compliant cover sheets
    cover sheet template has also been moved out-of-tree

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
     <p align="center">The material safety data sheet API.</p>
 </div>
 
+It's required that you load the necesary submodules to be able to template cover sheets.
+```sh
+git submodule update --init --recursive
+```
+
 ## License
 This work is licensed under MIT. Media assets in the `assets` directory are licensed under a
 Creative Commons Attribution-NoDerivatives 4.0 International Public License.
--- silicon/utils/sds.py
+++ silicon/utils/sds.py
@@ -4,10 +4,11 @@ from tungsten import SdsQueryFieldName, SigmaAldrichFieldMapper
 def get_sds_identifiers(sds_json: dict) -> dict[str, str | list[str]]:
     field_mapper = SigmaAldrichFieldMapper()
     return {
-        "product_name": field_mapper.getField(SdsQueryFieldName.PRODUCT_NAME, sds_json),
-        "product_brand": field_mapper.getField(SdsQueryFieldName.PRODUCT_BRAND, sds_json),
-        "product_number": field_mapper.getField(SdsQueryFieldName.PRODUCT_NUMBER, sds_json),
-        "cas_number": field_mapper.getField(SdsQueryFieldName.CAS_NUMBER, sds_json),
-        "signal_word": field_mapper.getField(SdsQueryFieldName.SIGNAL_WORD, sds_json),
-        "hazards": field_mapper.getField(SdsQueryFieldName.PICTOGRAM, sds_json),
+        "product_name": field_mapper.get_field(SdsQueryFieldName.PRODUCT_NAME, sds_json),
+        "product_brand": field_mapper.get_field(SdsQueryFieldName.PRODUCT_BRAND, sds_json),
+        "product_number": field_mapper.get_field(SdsQueryFieldName.PRODUCT_NUMBER, sds_json),
+        "cas_number": field_mapper.get_field(SdsQueryFieldName.CAS_NUMBER, sds_json),
+        "signal_word": field_mapper.get_field(SdsQueryFieldName.SIGNAL_WORD, sds_json),
+        "hazards": field_mapper.get_field(SdsQueryFieldName.PICTOGRAM, sds_json),
+        "statements": field_mapper.get_field(SdsQueryFieldName.STATEMENTS, sds_json),
     }
